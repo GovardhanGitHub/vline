6100bd27e936b00b36b91f580b472e3f9b73c889
govardhan <govardhan@gmail.com>
2025-06-20 22:59:59 +0530

    few dupe changes

--- src/utlits/fackData/projectData.jsx
+++ src/utlits/fackData/projectData.jsx
@@ -1,22 +1,7 @@
 // File: /utlits/fackData/projectData.js
 
 export const projectsData = [
-    {
-        id: 1,
-        category: "Healthcare",
-        title: "Why Antenatal Care Is Crucial During Pregnancy | Dr. Umadevi, MedStar Hospital Bengaluru",
-        description: "Join Dr. Umadevi, renowned gynecologist at MedStar Hospital Bengaluru, as she explains the vital role of antenatal care in ensuring a healthy pregnancy. Discover how regular checkups, screenings, and early interventions can prevent high-risk pregnancy complications and promote the well-being of both mother and baby. Don't miss this informative session designed for every expecting parent!",
-        youtubeId: "A0SzgvyY6C8",
-        client: "MedStar Hospital Bengaluru",
-    },
-    {
-        id: 2,
-        category: "Healthcare",
-        title: "Cervical Cancer Explained by Dr. Umadevi, Gynecologist at Medstar Speciality Hospital | Symptoms",
-        description: "Discover critical insights about cervical cancer from Dr. Umadevi, a leading gynecologist at Medstar Speciality Hospital, located at Kodigehalli Main Rd, Shanthivana, Sahakarnagar, Bengaluru, Karnataka 560092. In this comprehensive video, Dr. Umadevi breaks down the early warning signs, essential diagnostic tests, effective prevention strategies, and advanced treatment options for cervical cancer. Empower yourself with expert knowledge to prioritize your health and take proactive steps toward prevention and early detection.",
-        youtubeId: "t6Htn_WrEsg",
-        client: "Medstar Speciality Hospital Bengaluru",
-    },
+
     
 
 ];
